f58164f9eae44160ad89c1e5ed27f9d014d0163a
Ross Wright <Ross@Pross.co>
2023-02-11 15:45:18 -0600

    Tech POC Cleanup

--- JackHenry.TwitterScan.PocConsole/Program.cs
+++ JackHenry.TwitterScan.PocConsole/Program.cs
@@ -1,9 +1,12 @@
 ﻿using JackHenry.TwitterScan;
 using System.Text.Json;
 
+// Prompt user for tweet rate to be passed to the mock emitter
 int rate = 0;
 do
 {
+    // TODO: provide an option for chosing the live twitter feed
+
     Console.Write("Tweets Per Second (default 5600): ");
     var input = Console.ReadLine();
     if (string.IsNullOrWhiteSpace(input))
@@ -17,37 +20,39 @@ do
     }
 }while (rate == 0);
 
+// TODO: use the live twitter feed if it was selected above
+//var requestUri = "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=entities";
+var requestUri = $"https://localhost:7260/stream?rate={rate}";
+
+// output the header and prepare variables for loop
+Console.WriteLine("Elapsed      Count        Rate");
+var backspaces = new string('\b', 30);
+Console.Write(new string('*', 30));
+var count = 0;
+var jsonOpts = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+};
+//limit how often the UI is updated to reduce performance impact from console output
+var tweetsPerUiUpdate = rate > 1000 ? ( rate > 100000 ? 1000000 : 1000) : 1;   
+var tweetUnits = rate > 1000 ? (rate > 100000 ? "M" : "K") : " ";
+
+// open the stream
 using HttpClient httpClient = new HttpClient
 {
     Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite)
 };
-
-////var requestUri = "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=entities";
-var requestUri = $"https://localhost:7260/stream?rate={rate}";
-
 using var stream = await httpClient.GetStreamAsync(requestUri);
-
-Console.WriteLine("Elapsed      Count        Rate");
-var back = new string('\b', 30);
-Console.Write(new string('*', 30));
-
-var jsonOpts = new JsonSerializerOptions 
-{ 
-    PropertyNameCaseInsensitive= true,
-};
 using var reader = new StreamReader(stream);
 
-var count = 0;
+// process the stream
 var start = DateTime.UtcNow.Ticks;
-var step = rate > 1000 ? ( rate > 100000 ? 1000000 : 1000) : 1;
-var stepUnit = rate > 1000 ? (rate > 100000 ? "M" : "K") : " ";
-
 await foreach (var tweet in JsonSerializer.DeserializeAsyncEnumerable<Tweet>(stream, jsonOpts))
 {
     count++;
-    if (count % step == 0)
+    if (count % tweetsPerUiUpdate == 0)
     {
-        var sec = (double)(DateTime.UtcNow.Ticks - start) / TimeSpan.TicksPerSecond;
-        Console.Write($"{back}{(int)sec,6}s  {count/step,8}{stepUnit}  {(int)(count / sec),8}/s");
+        var secondsElapsed = (double)(DateTime.UtcNow.Ticks - start) / TimeSpan.TicksPerSecond;
+        Console.Write($"{backspaces}{(int)secondsElapsed,6}s  {count / tweetsPerUiUpdate,8}{tweetUnits}  {(int)(count / secondsElapsed),8}/s");
     }
 }
